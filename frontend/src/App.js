3fa71633f7b60f20f480379a8e3081678cb41ab8
1JigSaW <misho_1998@mail.ru>
2022-05-11 00:15:37 +0300

    update 1 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import React, {useEffect, useState}  from 'react'
+import React, {useEffect, useState, Component}  from 'react'
+import image from "./images/sample.jpg";
 
 function Orders(props) {
   const {order} = props
@@ -8,22 +9,109 @@ function Orders(props) {
 }
 
 
-function App() {
-  const [orders, setOrders] = useState([])
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      activeItem: {
+        title: "",
+        description: "",
+        completed: false
+      },
+      todoList: []
+      };
+  }
 
-  useEffect(() => {
-    const myCallback = (response, status) => {
-      console.log(response, status)
-      if (status === 200){
-        setOrders(response)
-      } else {
-        alert('Oh nO')
-      }
+    async componentDidMount() {
+      try {
+        const res = await fetch('http://localhost:8000/api/orders/');
+        const todoList = await res.json();
+        this.setState({
+          todoList
+        });
+      } catch (e) {
+        console.log(e);
     }
-  })
-  {orders.map((item, index)=>{
-    return <Orders order={item}/>
-  })}
-}
+    }
+    renderItems = () => {
+      const { viewCompleted } = this.state;
+      const newItems = this.state.todoList.filter(
+        item => item.completed === viewCompleted
+      );
+      return newItems.map(item => (
+        <li 
+          key={item.id}
+          className="list-group-item d-flex justify-content-between align-items-center"
+        >
+          <span 
+            className={`todo-title mr-2 ${
+              this.state.viewCompleted ? "completed-todo" : ""
+            }`}
+            title={item.description}
+            >
+              {item.title}
+            </span>
+        </li>
+      ));
+    };
 
+    render() {
+      return (
+        <div>
+          <header>
+            <nav className="navbar navbar-expand-lg navbar-light ">
+              <div className="container">
+                <a className="navbar-brand" href="#">ЛОГО</a>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul className="navbar-nav mb-2 mb-lg-0 ml-auto">
+                    <li className="nav-item dropdown">
+                      <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="./sample.jpg" className="circle" />
+                      </a>
+                      <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <li><a className="dropdown-item" href="#">Action</a></li>
+                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li><hr className="dropdown-divider" /></li>
+                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </header>
+
+          <main>
+            <div className="container">
+              <div className="content mt-3 mb-3 border border-dark">
+                <div className="m-3">
+                  <div class="input-group mb-3">
+                    <input type="text" class="form-control border border-dark" placeholder="Search" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                    <div class="input-group-append s_button">
+                      <button class="btn btn-outline-secondary border border-dark" type="button"><img src="{image}" className="circle" /></button>
+                    </div>
+                  </div>
+                </div>
+                  {/*<ul className="list-group list-group-flush">
+                    {this.renderItems()}
+                  </ul>*/}
+                <div className="m-3">
+                  <div class="row">
+                    <div class="col-9">
+                      One of three columns
+                    </div>
+                    <div class="col-3">
+                      One of three columns
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      )
+    }
+  }
+  
 export default App;
